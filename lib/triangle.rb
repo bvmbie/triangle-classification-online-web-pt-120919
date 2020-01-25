a58960435653ffbc35faa082af749fae8e91761a
Henna Vahatiitto <hnnamv@gmail.com>
2020-01-25 21:25:15 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,13 @@ class Triangle
   end
   
   def kind 
-    
+    if @side1 == @side2 && @side1 == @side3 && @side2 == @side3
+      return :equilateral
+    elsif @side1 == @side2 || @side2 == @side3 || @side3 == @side1
+      return :isosceles
+    else
+      return :scalene
+    end
   end
   
   class TriangleError < StandardError
